de9361276ddd98ce7fad2815e6f57b8701f9b44f
Rane Wallin <github@blasph.me>
2020-09-11 11:36:05 -0700

    Remove dropping collections

--- db/insertData.js
+++ db/insertData.js
@@ -17,16 +17,16 @@ const loadData = async () => {
 
   await setupCollections();
 
-  const trx = await db.beginTransaction({
+  const { id } = await db.beginTransaction({
     write: [games, traits, edges]
   });
+  const trx = db.transaction(id);
 
   for (let j = 0; j < files.length; j++) {
     let data = fs.readFileSync(path.resolve(__dirname, 'data', files[j]), 'utf8');
     let collection = files[j].match(/(.+?)(?=\d|\.)/)[0];
     data = await neatCsv(data);
     await insertData(data, collections[collection], trx);
-    console.log('inserted ' + files[j]);
   }
 
   trx.commit();
@@ -48,9 +48,9 @@ const setupCollections = async () => {
   let { games, traits, edges } = collections;
 
   try {
-    await games.drop();
-    await traits.drop();
-    await edges.drop();
+    // if (games) await games.drop();
+    // if (traits) await traits.drop();
+    // if (edges) await edges.drop();
 
     await games.create();
     await traits.create();
