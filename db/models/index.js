30b089ab89e54558aef86e85aa073152068cb4df
Rane Wallin <github@blasph.me>
2020-09-12 10:35:48 -0700

    Refactor fetch method to use Promise.all()

--- db/models/index.js
+++ db/models/index.js
@@ -1,6 +1,9 @@
 const db = require('../index.js');
 const { aql } = require('arangojs');
 
+const MAX_RECORDS = 10000;
+const NUM_GAMES_PER_TRAIT = 4;
+
 /**
  * Gets a list of traits for a product
  * @param {String} id The ID of a product for which to find thje traits
@@ -16,28 +19,43 @@ const fetchTraitsForProduct = async (id) => {
           return { id: doc._id, name: doc.name }
     `);
 
-  results = await results.map((trait) => trait);
+  results = await results.all();
+  console.log(results);
 
   return results;
 };
 
-const fetchProductsForTrait = (traitStr) => {
-  return new Promise((resolve, reject) => {
-    db.connection.query(fetchProductsForTraitQueryString, [traitStr], (err, products) => {
-      if (err) {
-        reject(err);
-      }
-      const productsArray = products.map((product) => {
-        return product.product_id;
-      });
+/**
+ * Takes in a trait ID and gets four products with the same trait
+ * for practical reasons the list of games is limited to 10k results
+ * and four are randomly selected from that
+ * @param {string} id - ID of a trait for which to find some randomish games
+ */
+const fetchProductsForTrait = async (trait, game) => {
+  const edgesCollection = db.collection('edges');
+  //const whichGames = getRandomGames();
+  let count = 0;
 
-      const productsObj = {
-        trait: traitStr,
-        products: productsArray
-      };
-      resolve(productsObj);
-    });
-  });
+  let results = await db.query(aql`
+    FOR edge IN ${edgesCollection}
+      FILTER edge._to == ${trait}
+      LIMIT 10000
+      return edge._from
+  `);
+  results = await results.all();
+  results = getRandomGames(results);
+
+  return results;
+};
+
+const getRandomGames = (results, id) => {
+  let games = [];
+
+  for (let i = 0; i < NUM_GAMES_PER_TRAIT; i++) {
+    games.push(results[Math.floor(Math.random() * MAX_RECORDS)]);
+  }
+
+  return games;
 };
 
 /**
