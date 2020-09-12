d8062aa92233740b5239c4a23cfb967e26d0a4eb
Rane Wallin <github@blasph.me>
2020-09-12 10:35:48 -0700

    Refactor fetch method to use Promise.all()

--- server/server.js
+++ server/server.js
@@ -7,11 +7,8 @@ require('dotenv').config({
 const express = require('express');
 const bodyParser = require('body-parser');
 const axios = require('axios');
-const fetchers = require('../database-sql/models');
 
-const buildData = require('../db/buildData');
 const db = require('../db/index');
-const insertMe = require('../db/insertData');
 
 const app = express();
 
@@ -26,73 +23,90 @@ app.get('/:product_id', (req, res) => {
   res.end();
 });
 
-app.get('/traits/:product_id', (req, res) => {
+app.get('/traits/:product_id', async (req, res) => {
   const id = +req.params.product_id;
-  const traits = fetchers.fetchTraitsForProduct(id);
+  const traits = await db.fetchTraitsForProduct(id);
+  let traitProducts = [];
 
-  traits.then((traitsData) => {
-    const traitProducts = traitsData.traits.reduce((acc, trait) => {
-      return acc.concat(fetchers.fetchProductsForTrait(trait, id));
-    }, []);
-    Promise.all(traitProducts)
-      .then((resultsFinal) => {
-        resultsFinal.forEach((result) => {
-          if (result.products.indexOf(id) >= 0) {
-            result.products = result.products.filter((product) => {
-              return product !== id;
-            });
-            // eslint-disable-next-line no-param-reassign
-          }
-          while (result.products.length < 4) {
-            const filler = Math.ceil(Math.random() * 100);
-            if (!result.products.includes(filler) && filler !== id) {
-              result.products.push(filler);
-            }
-          }
-        });
-        axios
-          .all(
-            resultsFinal.map((result) => {
-              console.log(result.products);
-              const requestArray = encodeURI(JSON.stringify(result.products));
-              const requestURL = `http://ec2-52-14-126-227.us-east-2.compute.amazonaws.com:3001/api/${requestArray}?type=thumbnail`;
+  if (!traits || traits.length === 0) {
+    return res.status(404).send('No traits found for this product.');
+  }
 
-              return axios.get(requestURL);
-            })
-          )
-          .then((resArray) => {
-            const productArray = resArray.map((response) => {
-              return response.data;
-            });
-            // eslint-disable-next-line no-plusplus
-            for (let i = 0; i < productArray.length; i++) {
-              if (
-                productArray[i].every((item) => {
-                  return resultsFinal[i].products.includes(item.product_id);
-                })
-              ) {
-                console.log('trait', resultsFinal[i].trait);
-                productArray[i].push(resultsFinal[i].trait);
-              }
-            }
-            console.log('From Micko ', productArray);
-            return productArray;
-          })
-          .then((resToClient) => {
-            res.set({ 'Access-Control-Allow-Origin': '*' });
-            res.status(200).send(resToClient);
-          })
-          .catch((err) => {
-            console.log(err);
-            res.status(500).send('there was a problem');
-          });
-      })
-      .catch((err) => {
-        if (err) {
-          throw err;
-        }
-      });
-  });
+  for (const { id } of traits) {
+    let products = await db.fetchProductsForTrait(trait.id, id);
+    traitProducts.push({
+      [trait.id]: {
+        name: trait.name,
+        products
+      }
+    });
+  }
+
+  res.status(200).send('OK');
+
+  // traits.then((traitsData) => {
+  //   const traitProducts = traitsData.traits.reduce((acc, trait) => {
+  //     return acc.concat(fetchers.fetchProductsForTrait(trait, id));
+  //   }, []);
+  //   Promise.all(traitProducts)
+  //     .then((resultsFinal) => {
+  //       resultsFinal.forEach((result) => {
+  //         if (result.products.indexOf(id) >= 0) {
+  //           result.products = result.products.filter((product) => {
+  //             return product !== id;
+  //           });
+  //           // eslint-disable-next-line no-param-reassign
+  //         }
+  //         while (result.products.length < 4) {
+  //           const filler = Math.ceil(Math.random() * 100);
+  //           if (!result.products.includes(filler) && filler !== id) {
+  //             result.products.push(filler);
+  //           }
+  //         }
+  //       });
+  //       axios
+  //         .all(
+  //           resultsFinal.map((result) => {
+  //             console.log(result.products);
+  //             const requestArray = encodeURI(JSON.stringify(result.products));
+  //             const requestURL = `http://ec2-52-14-126-227.us-east-2.compute.amazonaws.com:3001/api/${requestArray}?type=thumbnail`;
+
+  //             return axios.get(requestURL);
+  //           })
+  //         )
+  //         .then((resArray) => {
+  //           const productArray = resArray.map((response) => {
+  //             return response.data;
+  //           });
+  //           // eslint-disable-next-line no-plusplus
+  //           for (let i = 0; i < productArray.length; i++) {
+  //             if (
+  //               productArray[i].every((item) => {
+  //                 return resultsFinal[i].products.includes(item.product_id);
+  //               })
+  //             ) {
+  //               console.log('trait', resultsFinal[i].trait);
+  //               productArray[i].push(resultsFinal[i].trait);
+  //             }
+  //           }
+  //           console.log('From Micko ', productArray);
+  //           return productArray;
+  //         })
+  //         .then((resToClient) => {
+  //           res.set({ 'Access-Control-Allow-Origin': '*' });
+  //           res.status(200).send(resToClient);
+  //         })
+  //         .catch((err) => {
+  //           console.log(err);
+  //           res.status(500).send('there was a problem');
+  //         });
+  //     })
+  //     .catch((err) => {
+  //       if (err) {
+  //         throw err;
+  //       }
+  //     });
+  // });
 });
 
 app.get('/traits/products/:trait', (req, res) => {
